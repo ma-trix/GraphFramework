55feb8e3f960f585efa558982acd7952f32ff304
Mateusz Łukomski <matrix.disc@gmail.com>
2013-11-11 16:32:05 +0100

    First working test (new graph has no nodes)

---
+++ GraphFramework/GraphFramework/Graph.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphFramework
+{
+    public class Graph
+    {
+        public LinkedList<Node> nodes = new LinkedList<Node>();
+    }
+
+    public class Node
+    {
+    }
+
+    public class GraphGenerator
+    {
+        public Graph gerenarteRandomGraph()
+        {
+            Graph g = new Graph();
+
+            return g;
+        }
+    }
+}
---
+++ GraphFramework/GraphFramework/GraphUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace GraphFramework
+{
+    class GraphUnitTests
+    {
+        [Test]
+        public void EmptyGraphHasNoNodes()
+        {
+            Graph graph = new Graph();
+            Assert.AreEqual(graph.nodes.Count, 0);
+        }
+    }
+}
