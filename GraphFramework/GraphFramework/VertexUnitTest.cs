6f6c03ffdc363b0cea8d2982491a674bc6ab2335
Mateusz Łukomski <matrix.disc@gmail.com>
2013-11-14 15:43:55 +0100

    Renamed fields in vertex unit tests

--- GraphFramework/GraphFramework/VertexUnitTest.cs
+++ GraphFramework/GraphFramework/VertexUnitTest.cs
@@ -5,126 +5,125 @@ namespace GraphFramework
     [TestFixture]
     public class VertexUnitTest
     {
-        private Vertex _vertex1;
-        private Vertex _vertex2;
+        private Vertex _v1;
+        private Vertex _v2;
 
         [SetUp]
         public void Init()
         {
-            _vertex1 = new Vertex();
-            _vertex2 = new Vertex();
+            _v1 = new Vertex();
+            _v2 = new Vertex();
         }
 
         [Test]
         public void NewVertexHasNoNeighbours()
         {
-            Vertex vertex = new Vertex();
-            Assert.AreEqual(0, vertex.Neighbours.Count);
+            Assert.AreEqual(0, _v1.Neighbours.Count);
         }
 
         [Test]
         public void AddingEdgeMakesVerticesNeighboursOfEachOther()
         {
-            _vertex1.AddEdge(_vertex2);
-            Assert.Contains(_vertex2, _vertex1.Neighbours);
-            Assert.Contains(_vertex1, _vertex2.Neighbours);
-            Assert.Contains(_vertex1, _vertex2.Inbound);
-            Assert.Contains(_vertex2, _vertex1.Inbound);
+            _v1.AddEdge(_v2);
+            Assert.Contains(_v2, _v1.Neighbours);
+            Assert.Contains(_v1, _v2.Neighbours);
+            Assert.Contains(_v1, _v2.Inbound);
+            Assert.Contains(_v2, _v1.Inbound);
         }
 
         [Test]
         public void CanNotAddMultipleEdgesBetweenTwoVertices()
         {
-            _vertex1.AddEdge(_vertex2);
-            Assert.Throws<NoMultiedgePermitedException>(() => _vertex1.AddEdge(_vertex2));
+            _v1.AddEdge(_v2);
+            Assert.Throws<NoMultiedgePermitedException>(() => _v1.AddEdge(_v2));
         }
 
         [Test]
         public void AddsArcToVertex()
         {
-            _vertex1.AddArc(_vertex2);
-            Assert.Contains(_vertex2, _vertex1.Neighbours);
-            Assert.Contains(_vertex1, _vertex2.Inbound);
+            _v1.AddArc(_v2);
+            Assert.Contains(_v2, _v1.Neighbours);
+            Assert.Contains(_v1, _v2.Inbound);
         }
 
         [Test]
         public void AddingArcToVertexIsOneWayOnly()
         {
-            _vertex1.AddArc(_vertex2);
-            Assert.IsFalse(_vertex2.Neighbours.Contains(_vertex1));
-            Assert.IsFalse(_vertex1.Inbound.Contains(_vertex2));
+            _v1.AddArc(_v2);
+            Assert.IsFalse(_v2.Neighbours.Contains(_v1));
+            Assert.IsFalse(_v1.Inbound.Contains(_v2));
         }
         
         [Test]
         public void CanNotAddArcIfStartVertexIsAlreadyConsecutiveToEndVertex()
         {
-            _vertex1.AddArc(_vertex2);
-            Assert.Throws<NoMultiedgePermitedException>(() => _vertex1.AddArc(_vertex2));
+            _v1.AddArc(_v2);
+            Assert.Throws<NoMultiedgePermitedException>(() => _v1.AddArc(_v2));
         }
 
         [Test]
         public void CanNotAddEdgeIfEndVertexIsAlreadyConsecutiveToStartVertex()
         {
-            _vertex2.AddArc(_vertex1);
-            Assert.Throws<NoMultiedgePermitedException>(() => _vertex1.AddEdge(_vertex2));
+            _v2.AddArc(_v1);
+            Assert.Throws<NoMultiedgePermitedException>(() => _v1.AddEdge(_v2));
         }
 
         [Test]
         public void NewVertexHasOutDegreeZero()
         {
-            Assert.AreEqual(0, _vertex1.OutDegree);
+            Assert.AreEqual(0, _v1.OutDegree);
         }
 
         [Test]
         public void VertexKnowsItsNonzeroOutDegree()
         {
-            _vertex1.AddArc(_vertex2);
-            Assert.AreEqual(1, _vertex1.OutDegree);
+            _v1.AddArc(_v2);
+            Assert.AreEqual(1, _v1.OutDegree);
         }
 
         [Test]
         public void CanRemoveArcBetweenTwoVertices()
         {
-            _vertex1.AddArc(_vertex2);
-            _vertex1.RemoveArc(_vertex2);
-            Assert.IsFalse(_vertex1.Neighbours.Contains(_vertex2));
-            Assert.IsFalse(_vertex2.Inbound.Contains(_vertex1));
+            _v1.AddArc(_v2);
+            _v1.RemoveArc(_v2);
+            Assert.IsFalse(_v1.Neighbours.Contains(_v2));
+            Assert.IsFalse(_v2.Inbound.Contains(_v1));
         }
 
         [Test]
         public void RemovingNonExistentArcThrowsException()
         {
-            Assert.Throws<NoArcException>(() => _vertex1.RemoveArc(_vertex2));
+            Assert.Throws<NoArcException>(() => _v1.RemoveArc(_v2));
         }
 
         [Test]
         public void CanRemoveEdgeBetweenTwoVertices()
         {
-            _vertex1.AddEdge(_vertex2);
-            _vertex1.RemoveEdge(_vertex2);
-            Assert.IsFalse(_vertex1.Neighbours.Contains(_vertex2));
-            Assert.IsFalse(_vertex2.Neighbours.Contains(_vertex1));
-            Assert.IsFalse(_vertex1.Inbound.Contains(_vertex2));
-            Assert.IsFalse(_vertex2.Inbound.Contains(_vertex1));
+            _v1.AddEdge(_v2);
+            _v1.RemoveEdge(_v2);
+            Assert.IsFalse(_v1.Neighbours.Contains(_v2));
+            Assert.IsFalse(_v2.Neighbours.Contains(_v1));
+            Assert.IsFalse(_v1.Inbound.Contains(_v2));
+            Assert.IsFalse(_v2.Inbound.Contains(_v1));
         }
 
         [Test]
         public void RemovingNonExistenEdgeThrowsException()
         {
-            Assert.Throws<NoArcException>(() => _vertex1.RemoveEdge(_vertex2));
+            Assert.Throws<NoArcException>(() => _v1.RemoveEdge(_v2));
         }
 
         [Test]
         public void RemovingEdgeWhenArcFromEndVertexToStartVertexExistsThrowsException()
         {
-            _vertex2.AddArc(_vertex1);
-            Assert.Throws<NoArcException>(() => _vertex1.RemoveEdge(_vertex2));
+            _v2.AddArc(_v1);
+            Assert.Throws<NoArcException>(() => _v1.RemoveEdge(_v2));
         }
 
         [Test]
         public void NewVertexHasZeroInboundNeighbours()
         {
-            Assert.AreEqual(0, _vertex1.Inbound.Count);
+            Assert.AreEqual(0, _v1.Inbound.Count);
         }
     }
 }
