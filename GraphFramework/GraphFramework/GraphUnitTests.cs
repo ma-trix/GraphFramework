0176a6da1422d65307978ee821a38bd0a6f8083e
Mateusz Łukomski <matrix.disc@gmail.com>
2013-11-14 14:22:00 +0100

    Refactored GraphUnitTests to use fixture SetUp

--- GraphFramework/GraphFramework/GraphUnitTests.cs
+++ GraphFramework/GraphFramework/GraphUnitTests.cs
@@ -4,32 +4,37 @@ namespace GraphFramework
 {
     class GraphUnitTests
     {
+        private Graph _graph;
+        private Vertex _newVertex;
+
+        [SetUp]
+        public void Init()
+        {
+            _graph = new Graph();
+            _newVertex = new Vertex();
+        }
+
         [Test]
         public void EmptyGraphHasNoVertices()
         {
-            Graph graph = new Graph();
-            Assert.AreEqual(0, graph.vertices.Count);
+            Assert.AreEqual(0, _graph.vertices.Count);
         }
 
 
         [Test]
         public void AddsVertexToGraph()
         {
-            Graph graph = new Graph();
-            Vertex newVertex = new Vertex();
-            graph.AddVertex(newVertex);
-            Assert.Contains(newVertex, graph.vertices);
+            _graph.AddVertex(_newVertex);
+            Assert.Contains(_newVertex, _graph.vertices);
         }
 
         [Test]
         public void RemovesVertexFromGraph()
         {
-            Graph graph = new Graph();
-            Vertex newVertex = new Vertex();
-            graph.AddVertex(newVertex);
-            Assert.IsTrue(graph.vertices.Contains(newVertex));
-            graph.RemoveVertex(newVertex);
-            Assert.IsFalse(graph.vertices.Contains(newVertex));
+            _graph.AddVertex(_newVertex);
+            Assert.IsTrue(_graph.vertices.Contains(_newVertex));
+            _graph.RemoveVertex(_newVertex);
+            Assert.IsFalse(_graph.vertices.Contains(_newVertex));
         }
     }
 }
