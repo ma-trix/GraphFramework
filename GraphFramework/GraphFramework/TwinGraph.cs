3a75103730506144379e37e12bac9e62e1cb268e
Mateusz Łukomski <matrix.disc@gmail.com>
2013-11-20 14:16:09 +0100

    First unit tests of Twin Graph

---
+++ GraphFramework/GraphFramework/TwinGraph.cs
@@ -0,0 +1,24 @@
+﻿namespace GraphFramework
+{
+    public class TwinGraph
+    {
+        public Vertex StartVertex
+        {
+            get { return _startVertex; }
+        }
+
+        public Vertex EndVertex
+        {
+            get {return _endVertex; }
+        }
+
+        private readonly Vertex _startVertex;
+        private readonly Vertex _endVertex;
+
+        public TwinGraph()
+        {
+            _startVertex = new Vertex();
+            _endVertex = new Vertex();
+        }
+    }
+}
---
+++ GraphFramework/GraphFramework/TwinGraphUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace GraphFramework
+{
+    [TestFixture]
+    public class TwinGraphUnitTests
+    {
+        [Test]
+        public void NewTwinGraphHasStartVertex()
+        {
+            TwinGraph tg = new TwinGraph();
+            Assert.NotNull(tg.StartVertex);
+        }
+
+        [Test]
+        public void NewTwinGraphHasEndVertex()
+        {
+            TwinGraph tg = new TwinGraph();
+            Assert.NotNull(tg.EndVertex);
+        }
+    }
+}
