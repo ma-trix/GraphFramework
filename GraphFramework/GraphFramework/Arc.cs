5e75da38c0ad31e99454ec8a89319f0c023e639e
Mateusz Łukomski <matrix.disc@gmail.com>
2013-12-05 14:18:15 +0100

    Added Arc class

---
+++ GraphFramework/GraphFramework/Arc.cs
@@ -0,0 +1,21 @@
+﻿namespace GraphFramework
+{
+    public class Arc
+    {
+        public Vertex Start;
+        public Vertex End;
+        private Graph graph;
+
+        public Arc(Graph graph, Vertex start, Vertex end)
+        {
+            Start = start;
+            End = end;
+            this.graph = graph;
+        }
+
+        public Graph Graph
+        {
+            get { return graph; }
+        }
+    }
+}
---
+++ GraphFramework/GraphFramework/ArcUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace GraphFramework
+{
+    [TestFixture]
+    public class ArcUnitTests
+    {
+        private Vertex v1;
+        private Vertex v2;
+        private Graph g;
+
+        [SetUp]
+        public void Init()
+        {
+            v1 = new Vertex();
+            v2 = new Vertex();
+            g = new Graph();
+        }
+
+        [Test]
+        public void SetsStartAndEndVerticesOnCreate()
+        {
+            Arc a = new Arc(null, v1, v2);
+            Assert.AreSame(v1, a.Start);
+            Assert.AreSame(v2, a.End);
+        }
+
+        [Test]
+        public void KnowsGraphItBelongsTo()
+        {
+            Arc a = new Arc(g, v1, v2);
+            Assert.AreSame(g, a.Graph);
+        }
+    }
+}
