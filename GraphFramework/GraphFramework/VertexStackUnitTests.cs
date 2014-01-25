0546b4b3b546530286c064fcf54ff291433d225d
Mateusz Łukomski <matrix.disc@gmail.com>
2014-01-25 16:12:13 +0100

    Refactored to SetUp in VertexStackUnitTests

--- GraphFramework/GraphFramework/VertexStackUnitTests.cs
+++ GraphFramework/GraphFramework/VertexStackUnitTests.cs
@@ -5,49 +5,50 @@ namespace GraphFramework
     [TestFixture]
     public class VertexStackUnitTests
     {
+        private VertexStack k;
+        private ABVertex v1;
+        private ABVertex v2;
+
+        [SetUp]
+        public void SetUp()
+        {
+            k = new VertexStack();
+            v1 = new ABVertex(VertexType.A);
+            v2 = new ABVertex(VertexType.B);
+        }
+
         [Test]
         public void TopElementIsNullForEmptyVertexStack()
         {
-            var k = new VertexStack();
             Assert.That(k.Top(), Is.Null);
         }
 
         [Test]
         public void PushingElementSetsItToTop()
         {
-            var k = new VertexStack();
-            var v = new ABVertex(VertexType.A);
-            k.Push(v);
-            Assert.That(k.Top(), Is.SameAs(v));
+            k.Push(v1);
+            Assert.That(k.Top(), Is.SameAs(v1));
         }
 
         [Test]
         public void PushReturnsWrappedPushedElement()
         {
-            var k = new VertexStack();
-            var v = new ABVertex(VertexType.A);
-            var pushed = k.Push(v);
-            Assert.That(pushed.Value, Is.SameAs(v));
+            var pushed = k.Push(v1);
+            Assert.That(pushed.Value, Is.SameAs(v1));
             Assert.That(pushed.Ancestor, Is.Null);
         }
 
         [Test]
         public void PushingAnotherElementMakesCurrentTopItsAncestor()
         {
-            var k = new VertexStack();
-            var v = new ABVertex(VertexType.A);
-            k.Push(v);
-            var v2 = new ABVertex(VertexType.B);
-            var pushed = k.Push(v2);
-            Assert.That(pushed.Ancestor, Is.SameAs(v));
+            k.Push(v1);
+            var pushedV2 = k.Push(v2);
+            Assert.That(pushedV2.Ancestor, Is.SameAs(v1));
         }
 
         [Test]
         public void PushingAnotherElementMakesItDescendantOfCurrentTop()
         {
-            var k = new VertexStack();
-            var v1 = new ABVertex(VertexType.A);
-            var v2 = new ABVertex(VertexType.B);
             var pushedV1 = k.Push(v1);
             k.Push(v2);
             Assert.That(VertexHelper.DoesABVertexExist(v2.Guid, pushedV1.Descendants), Is.True);
