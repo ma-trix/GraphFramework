0eb69488bc900fd9b8973d9b9b5f461ea26e4a4b
Mateusz Łukomski <matrix.disc@gmail.com>
2014-03-12 14:06:38 +0100

    Refactored VertexUnitTests to fit new unit tests standard

--- GraphFramework/GraphFramework/VertexUnitTest.cs
+++ GraphFramework/GraphFramework/VertexUnitTest.cs
@@ -16,225 +16,244 @@ namespace GraphFramework
             _v2 = new Vertex();
         }
 
-        [Test]
-        public void AddingEdgeMakesVerticesNeighboursOfEachOther()
+        public class TheConstructor : VertexUnitTest
         {
-            _v1.AddEdge(_v2);
-            Assert.AreEqual(1, _v1.OutboundArcs.Select(arc => arc.Start == _v1 && arc.End == _v2).Count());
-            Assert.AreEqual(1, _v1.InboundArcs.Select(arc => arc.Start == _v1 && arc.End == _v2).Count());
-            Assert.AreEqual(1, _v2.OutboundArcs.Select(arc => arc.Start == _v1 && arc.End == _v2).Count());
-            Assert.AreEqual(1, _v2.InboundArcs.Select(arc => arc.Start == _v1 && arc.End == _v2).Count());
+
+            [Test]
+            public void NewVertexHasOutDegreeZero()
+            {
+                Assert.AreEqual(0, _v1.OutDegree);
+            }
+
+            [Test]
+            public void NewVertexHasInDegreeZero()
+            {
+                Assert.AreEqual(0, _v1.InDegree);
+            }
+
+            [Test]
+            public void NewVertexDoesntBelongToAnyGraph()
+            {
+                Assert.IsNull(_v1.Graph);
+            }
+
+            [Test]
+            public void NewVertexHasGuid()
+            {
+                Assert.IsNotNull(_v1.Guid);
+            }
+
+            [Test]
+            public void DefualtNameIsGuid()
+            {
+                Assert.That(_v1.Name, Is.EqualTo(_v1.Guid.ToString()));
+            }
+
+            [Test]
+            public void NamesAVertex()
+            {
+                Vertex v = new Vertex("1");
+                Assert.That(v.Name, Is.EqualTo("1"));
+            }
+
+            [Test]
+            public void NewVertexHasZeroOutboundEdges()
+            {
+                Assert.AreEqual(0, _v1.OutboundArcs.Count);
+            }
+
+            [Test]
+            public void NewVertexHasZeroInboundEdges()
+            {
+                Assert.AreEqual(0, _v1.InboundArcs.Count);
+            }
         }
 
-        [Test]
-        public void CanNotAddMultipleEdgesBetweenTwoVertices()
+        public class TheAddEdgeMethod : VertexUnitTest
         {
-            _v1.AddEdge(_v2);
-            Assert.Throws<NoMultiedgePermitedException>(() => _v1.AddEdge(_v2));
+            [Test]
+            public void AddingEdgeMakesVerticesNeighboursOfEachOther()
+            {
+                _v1.AddEdge(_v2);
+                Assert.AreEqual(1, _v1.OutboundArcs.Select(arc => arc.Start == _v1 && arc.End == _v2).Count());
+                Assert.AreEqual(1, _v1.InboundArcs.Select(arc => arc.Start == _v1 && arc.End == _v2).Count());
+                Assert.AreEqual(1, _v2.OutboundArcs.Select(arc => arc.Start == _v1 && arc.End == _v2).Count());
+                Assert.AreEqual(1, _v2.InboundArcs.Select(arc => arc.Start == _v1 && arc.End == _v2).Count());
+            }
+
+            [Test]
+            public void CanNotAddMultipleEdgesBetweenTwoVertices()
+            {
+                _v1.AddEdge(_v2);
+                Assert.Throws<NoMultiedgePermitedException>(() => _v1.AddEdge(_v2));
+            }
         }
 
-        [Test]
-        public void AddsOutboundArcToVertex()
+        public class TheRemoveArcMethod : VertexUnitTest
         {
-            Arc a = _v1.AddOutboundArc(_v2);
-            Assert.That(_v1.OutboundArcs.Contains(a), Is.True);
-            Assert.That(_v2.InboundArcs.Contains(a), Is.True);
+            [Test]
+            public void CanRemoveArcBetweenTwoVertices()
+            {
+                _v1.AddOutboundArc(_v2);
+                _v1.RemoveArc(_v2);
+                Assert.AreEqual(0, _v1.OutboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
+                Assert.AreEqual(0, _v2.InboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
+            }
+
+            [Test]
+            public void RemovingNonExistentArcThrowsException()
+            {
+                Assert.Throws<NoArcException>(() => _v1.RemoveArc(_v2));
+            }
         }
 
-        [Test]
-        public void AddingArcToVertexIsOneWayOnly()
+        public class TheRemoveEdgeMethod : VertexUnitTest
         {
-            _v1.AddOutboundArc(_v2);
-            Assert.That(_v1.InDegree, Is.EqualTo(0));
-            Assert.That(_v2.OutDegree, Is.EqualTo(0));
+            [Test]
+            public void CanRemoveEdgeBetweenTwoVertices()
+            {
+                _v1.AddEdge(_v2);
+                _v1.RemoveEdge(_v2);
+                Assert.AreEqual(0, _v1.OutboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
+                Assert.AreEqual(0, _v1.InboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
+                Assert.AreEqual(0, _v2.OutboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
+                Assert.AreEqual(0, _v2.InboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
+            }
+
+            [Test]
+            public void RemovingNonExistenEdgeThrowsException()
+            {
+                Assert.Throws<NoArcException>(() => _v1.RemoveEdge(_v2));
+            }
+
+            [Test]
+            public void RemovingEdgeWhenArcFromEndVertexToStartVertexExistsThrowsException()
+            {
+                _v2.AddOutboundArc(_v1);
+                Assert.Throws<NoArcException>(() => _v1.RemoveEdge(_v2));
+            }
+        }
+
+        public class TheAddOutboundArcOldMethod : VertexUnitTest
+        {
+            [Test]
+            public void AddsOutboundArcToVertex()
+            {
+                Arc a = _v1.AddOutboundArc(_v2);
+                Assert.That(_v1.OutboundArcs.Contains(a), Is.True);
+                Assert.That(_v2.InboundArcs.Contains(a), Is.True);
+            }
+
+            [Test]
+            public void AddingArcToVertexIsOneWayOnly()
+            {
+                _v1.AddOutboundArc(_v2);
+                Assert.That(_v1.InDegree, Is.EqualTo(0));
+                Assert.That(_v2.OutDegree, Is.EqualTo(0));
+            }
+
+            [Test]
+            public void CanNotAddArcIfStartVertexIsAlreadyConsecutiveToEndVertex()
+            {
+                _v1.AddOutboundArc(_v2);
+                Assert.Throws<NoMultiedgePermitedException>(() => _v1.AddOutboundArc(_v2));
+            }
+
+            [Test]
+            public void CanNotAddEdgeIfEndVertexIsAlreadyConsecutiveToStartVertex()
+            {
+                _v2.AddOutboundArc(_v1);
+                Assert.Throws<NoMultiedgePermitedException>(() => _v1.AddEdge(_v2));
+            }
+
+            [Test]
+            public void VertexKnowsItsNonzeroOutDegree()
+            {
+                _v1.AddOutboundArc(_v2);
+                Assert.AreEqual(1, _v1.OutDegree);
+            }
+
+            [Test]
+            public void VertexKnowsItsNonzeroInDegree()
+            {
+                _v2.AddOutboundArc(_v1);
+                Assert.AreEqual(1, _v1.InDegree);
+            }
+
+            [Test]
+            public void VerticesWithArcBetweenThemReferToTheSameObject()
+            {
+                _v1.AddOutboundArc(_v2);
+                Assert.AreSame(_v1.OutboundArcs.First(arc => arc.Start == _v1 && arc.End == _v2),
+                                _v2.InboundArcs.First(arc => arc.Start == _v1 && arc.End == _v2));
+            }
         }
         
-        [Test]
-        public void CanNotAddArcIfStartVertexIsAlreadyConsecutiveToEndVertex()
+        public class TheAddOutboundArcMethod : VertexUnitTest
         {
-            _v1.AddOutboundArc(_v2);
-            Assert.Throws<NoMultiedgePermitedException>(() => _v1.AddOutboundArc(_v2));
-        }
+            [Test]
+            public void AddsArcInMatchingToOutboundArcsInStartVertex()
+            {
+                _v1.AddOutboundArc(_v2, true);
+                var a = ArcHelper.FindArc(_v1, _v2, _v1.OutboundArcs);
+                Assert.That(a, Is.Not.Null);
+                Assert.That(a.IsInMatching, Is.True);
+            }
 
-        [Test]
-        public void CanNotAddEdgeIfEndVertexIsAlreadyConsecutiveToStartVertex()
-        {
-            _v2.AddOutboundArc(_v1);
-            Assert.Throws<NoMultiedgePermitedException>(() => _v1.AddEdge(_v2));
-        }
+            [Test]
+            public void AddsArcInMatchingToInboundArcsInEndVertex()
+            {
+                _v1.AddOutboundArc(_v2, true);
+                var a = ArcHelper.FindArc(_v1, _v2, _v2.InboundArcs);
+                Assert.That(a, Is.Not.Null);
+                Assert.That(a.IsInMatching, Is.True);
+            }
 
-        [Test]
-        public void NewVertexHasOutDegreeZero()
-        {
-            Assert.AreEqual(0, _v1.OutDegree);
-        }
+            [Test]
+            public void AddingArcInMatchingMakesStartVertexInMatching()
+            {
+                _v1.AddOutboundArc(_v2, true);
+                Assert.That(_v1.IsInMatching, Is.True);
+            }
 
-        [Test]
-        public void VertexKnowsItsNonzeroOutDegree()
-        {
-            _v1.AddOutboundArc(_v2);
-            Assert.AreEqual(1, _v1.OutDegree);
-        }
+            [Test]
+            public void AddingArcInMatchingMakesEndVertexInMatching()
+            {
+                _v1.AddOutboundArc(_v2, true);
+                Assert.That(_v2.IsInMatching, Is.True);
+            }
 
-        [Test]
-        public void NewVertexHasInDegreeZero()
-        {
-            Assert.AreEqual(0, _v1.InDegree);
-        }
+            [Test]
+            public void AddsArcNotInMatchingToOutboundArcsInStartVertex()
+            {
+                _v1.AddOutboundArc(_v2, false);
+                var a = ArcHelper.FindArc(_v1, _v2, _v1.OutboundArcs);
+                Assert.That(a, Is.Not.Null);
+                Assert.That(a.IsInMatching, Is.False);
+            }
 
-        [Test]
-        public void VertexKnowsItsNonzeroInDegree()
-        {
-            _v2.AddOutboundArc(_v1);
-            Assert.AreEqual(1, _v1.InDegree);
-        }
+            [Test]
+            public void AddsArcNotInMatchingToInboundArcsInEndVertex()
+            {
+                _v1.AddOutboundArc(_v2, false);
+                var a = ArcHelper.FindArc(_v1, _v2, _v2.InboundArcs);
+                Assert.That(a, Is.Not.Null);
+                Assert.That(a.IsInMatching, Is.False);
+            }
 
-        [Test]
-        public void CanRemoveArcBetweenTwoVertices()
-        {
-            _v1.AddOutboundArc(_v2);
-            _v1.RemoveArc(_v2);
-            Assert.AreEqual(0, _v1.OutboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
-            Assert.AreEqual(0, _v2.InboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2 )).Count());
-        }
+            [Test]
+            public void AddingArcNotInMatchingMakesStartVertexInMatching()
+            {
+                _v1.AddOutboundArc(_v2, false);
+                Assert.That(_v1.IsInMatching, Is.False);
+            }
 
-        [Test]
-        public void RemovingNonExistentArcThrowsException()
-        {
-            Assert.Throws<NoArcException>(() => _v1.RemoveArc(_v2));
-        }
-
-        [Test]
-        public void CanRemoveEdgeBetweenTwoVertices()
-        {
-            _v1.AddEdge(_v2);
-            _v1.RemoveEdge(_v2);
-            Assert.AreEqual(0, _v1.OutboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
-            Assert.AreEqual(0, _v1.InboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
-            Assert.AreEqual(0, _v2.OutboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
-            Assert.AreEqual(0, _v2.InboundArcs.Select(arc => (arc.Start == _v1 && arc.End == _v2)).Count());
-        }
-
-        [Test]
-        public void RemovingNonExistenEdgeThrowsException()
-        {
-            Assert.Throws<NoArcException>(() => _v1.RemoveEdge(_v2));
-        }
-
-        [Test]
-        public void RemovingEdgeWhenArcFromEndVertexToStartVertexExistsThrowsException()
-        {
-            _v2.AddOutboundArc(_v1);
-            Assert.Throws<NoArcException>(() => _v1.RemoveEdge(_v2));
-        }
-    
-        [Test]
-        public void NewVertexHasZeroOutboundEdges()
-        {
-            Assert.AreEqual(0, _v1.OutboundArcs.Count);
-        }
-
-        [Test]
-        public void NewVertexHasZeroInboundEdges()
-        {
-            Assert.AreEqual(0, _v1.InboundArcs.Count);
-        }
-
-        [Test]
-        public void VerticesWithArcBetweenThemReferToTheSameObject()
-        {
-            _v1.AddOutboundArc(_v2);
-            Assert.AreSame(_v1.OutboundArcs.First(arc => arc.Start == _v1 && arc.End == _v2),
-                            _v2.InboundArcs.First(arc => arc.Start == _v1 && arc.End == _v2));
-        }
-
-        [Test]
-        public void NewVertexDoesntBelongToAnyGraph()
-        {
-            Assert.IsNull(_v1.Graph);
-        }
-
-        [Test]
-        public void NewVertexHasGuid()
-        {
-            Assert.IsNotNull(_v1.Guid);
-        }
-
-        [Test]
-        public void DefualtNameIsGuid()
-        {
-            Assert.That(_v1.Name, Is.EqualTo(_v1.Guid.ToString()));
-        }
-
-        [Test]
-        public void NamesAVertex()
-        {
-            Vertex v = new Vertex("1");
-            Assert.That(v.Name, Is.EqualTo("1"));
-        }
-
-        [Test]
-        public void AddsArcInMatchingToOutboundArcsInStartVertex()
-        {
-            _v1.AddOutboundArc(_v2, true);
-            var a = ArcHelper.FindArc(_v1, _v2, _v1.OutboundArcs);
-            Assert.That(a, Is.Not.Null);
-            Assert.That(a.IsInMatching, Is.True);
-        }
-
-        [Test]
-        public void AddsArcInMatchingToInboundArcsInEndVertex()
-        {
-            _v1.AddOutboundArc(_v2, true);
-            var a = ArcHelper.FindArc(_v1, _v2, _v2.InboundArcs);
-            Assert.That(a, Is.Not.Null);
-            Assert.That(a.IsInMatching, Is.True);
-        }
-
-        [Test]
-        public void AddingArcInMatchingMakesStartVertexInMatching()
-        {
-            _v1.AddOutboundArc(_v2, true);
-            Assert.That(_v1.IsInMatching, Is.True);
-        }
-
-        [Test]
-        public void AddingArcInMatchingMakesEndVertexInMatching()
-        {
-            _v1.AddOutboundArc(_v2, true);
-            Assert.That(_v2.IsInMatching, Is.True);
-        }
-
-        [Test]
-        public void AddsArcNotInMatchingToOutboundArcsInStartVertex()
-        {
-            _v1.AddOutboundArc(_v2, false);
-            var a = ArcHelper.FindArc(_v1, _v2, _v1.OutboundArcs);
-            Assert.That(a, Is.Not.Null);
-            Assert.That(a.IsInMatching, Is.False);
-        }
-
-        [Test]
-        public void AddsArcNotInMatchingToInboundArcsInEndVertex()
-        {
-            _v1.AddOutboundArc(_v2, false);
-            var a = ArcHelper.FindArc(_v1, _v2, _v2.InboundArcs);
-            Assert.That(a, Is.Not.Null);
-            Assert.That(a.IsInMatching, Is.False);
-        }
-
-        [Test]
-        public void AddingArcNotInMatchingMakesStartVertexInMatching()
-        {
-            _v1.AddOutboundArc(_v2, false);
-            Assert.That(_v1.IsInMatching, Is.False);
-        }
-
-        [Test]
-        public void AddingArcNotInMatchingMakesEndVertexInMatching()
-        {
-            _v1.AddOutboundArc(_v2, false);
-            Assert.That(_v2.IsInMatching, Is.False);
+            [Test]
+            public void AddingArcNotInMatchingMakesEndVertexInMatching()
+            {
+                _v1.AddOutboundArc(_v2, false);
+                Assert.That(_v2.IsInMatching, Is.False);
+            }
         }
     }
 }
