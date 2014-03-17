95776be2cc828fcb3eeecd4f9f494fae34e9dbb1
Mateusz Łukomski <matrix.disc@gmail.com>
2014-03-17 15:13:53 +0100

    Refactored TheConstructor0 tests in TwinGraphUnitTests

--- GraphFramework/GraphFramework/TwinGraphUnitTests.cs
+++ GraphFramework/GraphFramework/TwinGraphUnitTests.cs
@@ -15,7 +15,7 @@ namespace GraphFramework
         private TwinVertex _tv2;
 
         [SetUp]
-        public void SetUp()
+        public void Init()
         {
             _tg = new TwinGraph();
             _g = new Graph();
@@ -26,156 +26,163 @@ namespace GraphFramework
             _tv2 = new TwinVertex(_precursor, _tg);
         }
 
-        [Test]
-        public void NewTwinGraphHasStartVertex()
+        public class TheConstructor0 : TwinGraphUnitTests
         {
-            Assert.NotNull(_tg.StartVertex);
+            [Test]
+            public void NewTwinGraphHasStartVertex()
+            {
+                Assert.That(_tg.StartVertex, Is.Not.Null);
+            }
+
+            [Test]
+            public void NewTwinGraphHasEndVertex()
+            {
+                Assert.That(_tg.EndVertex, Is.Not.Null);
+            }
+
+            [Test]
+            public void NewTwinGraphHasNoVertices()
+            {
+                Assert.That(_tg.Vertices, Is.Empty);
+            }
+
+            [Test]
+            public void NewTwinGraphHasNoArcs()
+            {
+                Assert.That(_tg.Arcs, Is.Empty);
+            }
         }
 
-        [Test]
-        public void NewTwinGraphHasEndVertex()
+        public class TheOtherMethods : TwinGraphUnitTests
         {
-            Assert.NotNull(_tg.EndVertex);
-        }
+            [Test]
+            public void TwinGraphCreatedFromGraphHasAllEdgesBasedOnPrecursors()
+            {
+                _g.AddVertex(_v1);
+                _g.AddVertex(_v2);
+                _g.AddEdge(_v1, _v2, false);
+                var tg = new TwinGraph(_g);
+                var tv1 = tg.Vertices.FirstOrDefault(tv => tv.Precursor.Guid == _v1.Guid);
+                var tv2 = tg.Vertices.FirstOrDefault(tv => tv.Precursor.Guid == _v2.Guid);
+                Assert.That(tv1, Is.Not.Null);
+                Assert.That(tv2, Is.Not.Null);
+                Assert.That(ArcHelper.DoesArcExist(tv1.B, tv2.A, tg.Arcs), Is.True);
+                Assert.That(ArcHelper.DoesArcExist(tv2.B, tv1.A, tg.Arcs), Is.True);
+            }
 
-        [Test]
-        public void EmptyTwinGraphHasNoVertices()
-        {
-            Assert.AreEqual(0, _tg.Vertices.Count);
-        }
+            [Test]
+            public void TwinGraphCreatedFromGraphHasAllTwinVerticesBasedOnPrecursors()
+            {
+                _g.AddVertex(_v1);
+                _g.AddVertex(_v2);
+                var tg = new TwinGraph(_g);
+                Assert.AreEqual(2, tg.Vertices.Count);
+                Assert.IsTrue(VertexHelper.DoesTwinVertexExist(_v1.Guid, tg.Vertices));
+                Assert.IsTrue(VertexHelper.DoesTwinVertexExist(_v2.Guid, tg.Vertices));
+            }
 
-        [Test]
-        public void AddsTwinVertexToTwinGraph()
-        {
-            _tg.AddTwinVertex(_tv1);
-            Assert.Contains(_tv1, _tg.Vertices);
-        }
+            [Test]
+            public void AddsTwinVertexToTwinGraph()
+            {
+                _tg.AddTwinVertex(_tv1);
+                Assert.Contains(_tv1, _tg.Vertices);
+            }
 
-        [Test]
-        public void TwinGraphCreatedFromGraphHasAllTwinVerticesBasedOnPrecursors()
-        {
-            _g.AddVertex(_v1);
-            _g.AddVertex(_v2);
-            var tg = new TwinGraph(_g);
-            Assert.AreEqual(2, tg.Vertices.Count);
-            Assert.IsTrue(VertexHelper.DoesTwinVertexExist(_v1.Guid, tg.Vertices));
-            Assert.IsTrue(VertexHelper.DoesTwinVertexExist(_v2.Guid, tg.Vertices));
-        }
 
-        [Test]
-        public void NewTwinGraphHasNoArcs()
-        {
-            Assert.That(_tg.Arcs.Count, Is.EqualTo(0));
-        }
+            [Test]
+            public void AddsEdgeToTwinGraph()
+            {
+                _tg.AddTwinVertex(_tv1);
+                _tg.AddTwinVertex(_tv2);
+                _tg.AddEdge(_tv1, _tv2, false);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tg.Arcs), Is.True);
+                Assert.That(ArcHelper.DoesArcExist(_tv2.B, _tv1.A, _tg.Arcs), Is.True);
+            }
 
-        [Test]
-        public void AddsEdgeToTwinGraph()
-        {
-            _tg.AddTwinVertex(_tv1);
-            _tg.AddTwinVertex(_tv2);
-            _tg.AddEdge(_tv1, _tv2, false);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tg.Arcs), Is.True);
-            Assert.That(ArcHelper.DoesArcExist(_tv2.B, _tv1.A, _tg.Arcs), Is.True);
-        }
+            [Test]
+            public void AddsArcToTwinGraph()
+            {
+                _tg.AddTwinVertex(_tv1);
+                _tg.AddTwinVertex(_tv2);
+                _tg.AddArc(_tv1, _tv2, false);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tg.Arcs), Is.True);
+                Assert.That(ArcHelper.DoesArcExist(_tv2.B, _tv1.A, _tg.Arcs), Is.False);
+            }
 
-        [Test]
-        public void AddsArcToTwinGraph()
-        {
-            _tg.AddTwinVertex(_tv1);
-            _tg.AddTwinVertex(_tv2);
-            _tg.AddArc(_tv1, _tv2, false);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tg.Arcs), Is.True);
-            Assert.That(ArcHelper.DoesArcExist(_tv2.B, _tv1.A, _tg.Arcs), Is.False);
-        }
+            [Test]
+            public void RemovesTwinVertexFromTwinGraph()
+            {
+                _tg.AddTwinVertex(_tv1);
+                Assert.That(_tg.Vertices.Contains(_tv1), Is.True);
+                _tg.RemoveTwinVertex(_tv1);
+                Assert.That(_tg.Vertices.Contains(_tv1), Is.False);
+            }
 
-        [Test]
-        public void TwinGraphCreatedFromGraphHasAllEdgesBasedOnPrecursors()
-        {
-            _g.AddVertex(_v1);
-            _g.AddVertex(_v2);
-            _g.AddEdge(_v1, _v2, false);
-            var tg = new TwinGraph(_g);
-            var tv1 = tg.Vertices.FirstOrDefault(tv => tv.Precursor.Guid == _v1.Guid);
-            var tv2 = tg.Vertices.FirstOrDefault(tv => tv.Precursor.Guid == _v2.Guid);
-            Assert.That(tv1, Is.Not.Null);
-            Assert.That(tv2, Is.Not.Null);
-            Assert.That(ArcHelper.DoesArcExist(tv1.B, tv2.A, tg.Arcs), Is.True);
-            Assert.That(ArcHelper.DoesArcExist(tv2.B, tv1.A, tg.Arcs), Is.True);
-        }
+            [Test]
+            public void RemovesNonMatchingArcBetweenTwinVertices()
+            {
+                _tg.AddTwinVertex(_tv1);
+                _tg.AddTwinVertex(_tv2);
+                _tg.AddArc(_tv1, _tv2, false);
+                _tg.RemoveArc(_tv1, _tv2, false);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tg.Arcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tv1.B.OutboundArcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tv2.A.InboundArcs), Is.False);
+            }
 
-        [Test]
-        public void RemovesTwinVertexFromTwinGraph()
-        {
-            _tg.AddTwinVertex(_tv1);
-            Assert.That(_tg.Vertices.Contains(_tv1), Is.True);
-            _tg.RemoveTwinVertex(_tv1);
-            Assert.That(_tg.Vertices.Contains(_tv1), Is.False);
-        }
+            [Test]
+            public void RemovesInMatchingArcBetweenTwinVertices()
+            {
+                _tg.AddTwinVertex(_tv1);
+                _tg.AddTwinVertex(_tv2);
+                _tg.AddArc(_tv1, _tv2, true);
+                _tg.RemoveArc(_tv1, _tv2, true);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tg.Arcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tv1.A.OutboundArcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tv2.B.InboundArcs), Is.False);
+            }
 
-        [Test]
-        public void RemovesNonMatchingArcBetweenTwinVertices()
-        {
-            _tg.AddTwinVertex(_tv1);
-            _tg.AddTwinVertex(_tv2);
-            _tg.AddArc(_tv1, _tv2, false);
-            _tg.RemoveArc(_tv1, _tv2, false);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tg.Arcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tv1.B.OutboundArcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tv2.A.InboundArcs), Is.False);
-        }
+            [Test]
+            public void RemovingNonexistentArcThrowsException()
+            {
+                Assert.Throws<NoArcException>(() => _tg.RemoveArc(_tv1, _tv2, false));
+            }
 
-        [Test]
-        public void RemovesInMatchingArcBetweenTwinVertices()
-        {
-            _tg.AddTwinVertex(_tv1);
-            _tg.AddTwinVertex(_tv2);
-            _tg.AddArc(_tv1, _tv2, true);
-            _tg.RemoveArc(_tv1, _tv2, true);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tg.Arcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tv1.A.OutboundArcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tv2.B.InboundArcs), Is.False);
-        }
+            [Test]
+            public void RemovingNonexistentTwinVertexThrowsException()
+            {
+                Assert.Throws<NoVertexException>(() => _tg.RemoveTwinVertex(_tv1));
+            }
 
-        [Test]
-        public void RemovingNonexistentArcThrowsException()
-        {
-            Assert.Throws<NoArcException>(() => _tg.RemoveArc(_tv1, _tv2, false));
-        }
+            [Test]
+            public void RemovesNonMatchingEdgeBetweenTwinVertices()
+            {
+                _tg.AddTwinVertex(_tv1);
+                _tg.AddTwinVertex(_tv2);
+                _tg.AddEdge(_tv1, _tv2, false);
+                _tg.RemoveEdge(_tv1, _tv2, false);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tg.Arcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tv1.B.OutboundArcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tv2.A.InboundArcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv2.B, _tv1.A, _tg.Arcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv2.B, _tv1.A, _tv2.B.OutboundArcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv2.B, _tv1.A, _tv1.A.InboundArcs), Is.False);
+            }
 
-        [Test]
-        public void RemovingNonexistentTwinVertexThrowsException()
-        {
-            Assert.Throws<NoVertexException>(() => _tg.RemoveTwinVertex(_tv1));
-        }
-
-        [Test]
-        public void RemovesNonMatchingEdgeBetweenTwinVertices()
-        {
-            _tg.AddTwinVertex(_tv1);
-            _tg.AddTwinVertex(_tv2);
-            _tg.AddEdge(_tv1, _tv2, false);
-            _tg.RemoveEdge(_tv1, _tv2, false);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tg.Arcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tv1.B.OutboundArcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.B, _tv2.A, _tv2.A.InboundArcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv2.B, _tv1.A, _tg.Arcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv2.B, _tv1.A, _tv2.B.OutboundArcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv2.B, _tv1.A, _tv1.A.InboundArcs), Is.False);
-        }
-
-        [Test]
-        public void RemovesInMatchingEdgeBetweenTwinVertices()
-        {
-            _tg.AddTwinVertex(_tv1);
-            _tg.AddTwinVertex(_tv2);
-            _tg.AddEdge(_tv1, _tv2, true);
-            _tg.RemoveEdge(_tv1, _tv2, true);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tg.Arcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tv1.A.OutboundArcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tv2.B.InboundArcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv2.A, _tv1.B, _tg.Arcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv2.A, _tv1.B, _tv2.A.OutboundArcs), Is.False);
-            Assert.That(ArcHelper.DoesArcExist(_tv2.A, _tv1.B, _tv1.B.InboundArcs), Is.False);
+            [Test]
+            public void RemovesInMatchingEdgeBetweenTwinVertices()
+            {
+                _tg.AddTwinVertex(_tv1);
+                _tg.AddTwinVertex(_tv2);
+                _tg.AddEdge(_tv1, _tv2, true);
+                _tg.RemoveEdge(_tv1, _tv2, true);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tg.Arcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tv1.A.OutboundArcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv1.A, _tv2.B, _tv2.B.InboundArcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv2.A, _tv1.B, _tg.Arcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv2.A, _tv1.B, _tv2.A.OutboundArcs), Is.False);
+                Assert.That(ArcHelper.DoesArcExist(_tv2.A, _tv1.B, _tv1.B.InboundArcs), Is.False);
+            }
         }
     }
 }
