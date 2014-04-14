b5d36ac0c86ecfa5d61b9096f1ef2f6e0d918527
Mateusz Łukomski <matrix.disc@gmail.com>
2014-04-14 22:54:58 +0200

    Full example run with logging on 10 vertex graph

--- GraphFramework/ExampleGraphTests/ExampleGraph.cs
+++ GraphFramework/ExampleGraphTests/ExampleGraph.cs
@@ -88,5 +88,52 @@ namespace ExampleGraphTests
 
             return g;
         }
+
+        public TwinGraph GenerateExampleTwinGraph0()
+        {
+            Graph g = GenerateExampleGraph0();
+            Log.Info("Graph generated ===================================================");
+            TwinGraph tg = new TwinGraph(g);
+            return tg;
+        }
+
+        public Graph GenerateExampleGraph0()
+        {
+            Graph g = new Graph();
+            Vertex v1 = new Vertex("1");
+            Vertex v2 = new Vertex("2");
+            Vertex v3 = new Vertex("3");
+            Vertex v4 = new Vertex("4");
+            Vertex v5 = new Vertex("5");
+            Vertex v6 = new Vertex("6");
+            Vertex v7 = new Vertex("7");
+            Vertex v8 = new Vertex("8");
+            Vertex v9 = new Vertex("9");
+            Vertex v10 = new Vertex("10");
+            g.AddVertex(v1);
+            g.AddVertex(v2);
+            g.AddVertex(v3);
+            g.AddVertex(v4);
+            g.AddVertex(v5);
+            g.AddVertex(v6);
+            g.AddVertex(v7);
+            g.AddVertex(v8);
+            g.AddVertex(v9);
+            g.AddVertex(v10);
+
+            g.AddEdge(v1, v2, false);
+            g.AddEdge(v2, v3, false);
+            g.AddEdge(v3, v4, false);
+            g.AddEdge(v4, v5, false);
+            g.AddEdge(v5, v6, false);
+            g.AddEdge(v6, v7, false);
+            g.AddEdge(v7, v3, false);
+            g.AddEdge(v6, v8, false);
+            g.AddEdge(v8, v9, false);
+            g.AddEdge(v9, v1, false);
+            g.AddEdge(v2, v10, false);
+
+            return g;
+        }
     }
 }
--- GraphFramework/ExampleGraphTests/Program.cs
+++ GraphFramework/ExampleGraphTests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using GraphFramework;
 
 namespace ExampleGraphTests
@@ -7,6 +8,9 @@ namespace ExampleGraphTests
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger
     (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static LinkedList<Arc> _mAugmentingPath;
+
         static void Main(string[] args)
         {
             LoggingHelper lh = new LoggingHelper();
@@ -14,16 +18,50 @@ namespace ExampleGraphTests
             log.Info("=====================================================================");
             var e = new ExampleGraph();
             //e.Generate2VertexExampleGraph();
-            var tg = e.GenerateExampleTwinGraph();
+            var tg = e.GenerateExampleTwinGraph0();
             tg.LogVertices();
             tg.LogArcs();
-            var k = new ABVertexStack();
-            var l = new LinkedList<ABVertex>();
-            var mdfs = new MDFS(tg, k, l);
-            var maug = mdfs.Run();
-            tg.SymmetricDifferenceWith(maug);
-            tg.LogArcs();
-            tg.LogVertices();
+            _mAugmentingPath = new LinkedList<Arc>();
+            var i = 0;
+            var _mAugmentingPaths = new LinkedList<LinkedList<Arc>>();
+            while (true)
+            {
+                i++;
+                log.Info("ITERATION " + i + " =============================================");
+                var k = new ABVertexStack();
+                var l = new LinkedList<ABVertex>();
+                var mdfs = new MDFS(tg, k, l);
+                MDFS._step = 0;
+                
+                _mAugmentingPath = mdfs.Run();
+                if (_mAugmentingPath == null)
+                {
+                    break;
+                }
+                _mAugmentingPaths.AddLast(_mAugmentingPath);
+                tg.SymmetricDifferenceWith(_mAugmentingPath);
+                tg.LogArcs();
+                tg.LogVertices();   
+            }
+            LogPaths(_mAugmentingPaths);
+        }
+
+        private static void LogPaths(LinkedList<LinkedList<Arc>> mAugmentingPaths)
+        {
+            var i = 0;
+            foreach (var mAugmentingPath in mAugmentingPaths)
+            {
+                i++;
+                var sb = new StringBuilder();
+                sb.Append(i);
+                sb.Append(" | ");
+                foreach (var arc in mAugmentingPath)
+                {
+                    sb.Append(arc);
+                    sb.Append(", ");
+                }
+                log.Info(sb.ToString());
+            }
         }
     }
 }
