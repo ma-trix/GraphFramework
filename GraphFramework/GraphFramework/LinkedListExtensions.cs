15215dea032c51e2b31a261b7ff05c1995c23043
Mateusz Łukomski <matrix.disc@gmail.com>
2014-04-14 22:51:17 +0200

    Added RemoveAll linked list extension

--- GraphFramework/GraphFramework/LinkedListExtensions.cs
+++ GraphFramework/GraphFramework/LinkedListExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GraphFramework
 {
@@ -21,6 +22,31 @@ namespace GraphFramework
             {
                 source.AddBefore(first, item);
             }
+        }
+
+        public static int RemoveAll<T>(this LinkedList<T> list, Predicate<T> match)
+        {
+            if (list == null)
+            {
+                throw new ArgumentNullException("list");
+            }
+            if (match == null)
+            {
+                throw new ArgumentNullException("match");
+            }
+            var count = 0;
+            var node = list.First;
+            while (node != null)
+            {
+                var next = node.Next;
+                if (match(node.Value))
+                {
+                    list.Remove(node);
+                    count++;
+                }
+                node = next;
+            }
+            return count;
         }
     }
 }
