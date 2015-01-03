5d600d321c8cbeffe461886099092a430a570ef3
Mateusz Łukomski <matrix.disc@gmail.com>
2015-01-03 13:08:21 +0100

    Added Logging to EWGT

--- GraphFramework/ExampleWeightedGraphTests/Program.cs
+++ GraphFramework/ExampleWeightedGraphTests/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ExampleGraphTests;
 
 namespace ExampleWeightedGraphTests
 {
-    class Program
+    static class Program
     {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger
+    (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
+            var lh = new LoggingHelper();
+            lh.ClearLogFile();
+            Log.Info("=====================================================================");
         }
     }
 }
